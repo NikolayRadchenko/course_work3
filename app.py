a29d2ff2c4819134fab285603908b30d0db3d347
User <lenovnikolas@gmail.com>
2023-01-07 12:05:02 +0200

    Без звездочек

--- app.py
+++ app.py
@@ -1,30 +1,81 @@
+import json
+import logging
+import werkzeug.exceptions
+
 from flask import Flask, request, render_template
+
+from utils import get_posts_all, get_post_by_pk, get_comments_by_post_id, search_for_posts, get_posts_by_user
+
+logging.basicConfig(filename="api.log", level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+
 
 app = Flask(__name__)
 
 
+@app.errorhandler(werkzeug.exceptions.BadRequest)
+def handle_bad_request(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(werkzeug.exceptions.BadRequest)
+def handle_bad_request(e):
+    return render_template("500.html"), 500
+
+
+app.register_error_handler(404, handle_bad_request)
+app.register_error_handler(500, handle_bad_request)
+
+
 @app.route("/", methods=["GET"])
 def load_main_page():
-    return render_template("index.html")
+    posts_all = get_posts_all()
+    return render_template("index.html", posts=posts_all, tag=tag)
 
 
-# @app.route("/feed", method=["GET"])
-# def load_feed_page():
-#     pass
+@app.route("/post/<int:post_id>", methods=["GET", "POST"])
+def load_post(post_id):
+    post = get_post_by_pk(post_id)
+    comments_post = get_comments_by_post_id(post_id)
+    return render_template("post.html", ava=post["poster_avatar"], user_name=post["poster_name"],
+                           img_post=post["pic"], views=post["views_count"], post_id=post_id, post_text=post["content"],
+                           comments_count=len(comments_post), comments=comments_post, poster_name=post["poster_name"])
 
 
-# @app.route("/post", method=["GET", "POST"])
-# def load_post():
-#     pass
+@app.route("/search/", methods=["GET"])
+def search_page():
+    tag = request.args.get("tag")
+    posts = search_for_posts(tag)
+    return render_template("search.html", posts=posts, search_count=len(posts))
 
 
-# @app.route("/search/?s=<key_search>", method=["GET"])
-# def search_page():
-#     pass
+@app.route("/user/<user_name>", methods=["GET"])
+def load_user(user_name):
+    posts_by_user = get_posts_by_user(user_name)
+    return render_template("user-feed.html", posts=posts_by_user, user_name=user_name)
 
 
-# @app.route("users/<user_name>", method=["GET"])
-# def load_user():
-#     pass
+@app.route("/api/posts/", methods=["GET"])
+def list_posts_json():
+    logging.info(msg='Запрос /api/posts')
+    with open("./data/posts.json", encoding="utf-8") as file:
+        data = json.load(file)
+        if type(data) == list:
+            return json.dumps(data, ensure_ascii=False)
+        else:
+            return "Неудалось загрузить json файл"
+
+
+@app.route("/api/posts/<int:post_id>", methods=["GET"])
+def load_post_json(post_id):
+    logging.info(msg=f'Запрос /api/posts/{post_id}')
+    with open("./data/posts.json", encoding="utf-8") as file:
+        data = json.load(file)
+        for _post in data:
+            if type(_post) == dict:
+                if _post["pk"] == post_id:
+                    return json.dumps(_post, ensure_ascii=False)
+            else:
+                return "Не удалось загрузить json файл"
+
 
 app.run()
--- utils.py
+++ utils.py
@@ -1,20 +1,49 @@
+import json
+
+COMMENTS = "./data/comments.json"
+POSTS = "./data/posts.json"
 
 
 def get_posts_all():
-    pass
+    with open(POSTS, encoding='utf-8') as file:
+        posts_all = json.load(file)
+        tag = []
+        for post in posts_all:
+            for word_content in post["content"]:
+                if "#" in word_content:
+                    tag.append(word_content)
+        return posts_all
 
 
 def get_posts_by_user(user_name):
-    pass
+    post_by_user = []
+    posts_all = get_posts_all()
+    for post in posts_all:
+        if user_name.lower() == post["poster_name"].lower():
+            post_by_user.append(post)
+    return post_by_user
 
 
 def get_comments_by_post_id(post_id):
-    pass
+    with open(COMMENTS, encoding='utf-8') as file:
+        comments = json.load(file)
+        comments_by_id = []
+        for comments_post in comments:
+            if post_id == comments_post["post_id"]:
+                comments_by_id.append(comments_post)
+        return comments_by_id
 
 
-def search_for_posts(query):
-    pass
+def search_for_posts(tag):
+    posts_by_tag = []
+    for post in get_posts_all():
+        if tag.lower() in post["content"].lower():
+            posts_by_tag.append(post)
+    return posts_by_tag
 
 
 def get_post_by_pk(pk):
-    pass
+    posts_all = get_posts_all()
+    for post in posts_all:
+        if post["pk"] == pk:
+            return post
